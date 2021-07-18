31529228de493fe3cd826f79d162cbc14d3fe3eb
Liza Vakhrusheva <nyamnyannyak@yandex.ru>
2021-07-18 16:36:00 +0300

    limiter и helmet

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const routes = require('./routes/index');
 const { handleErrors } = require('./middlewares/errors-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, NODE_ENV, DATABASE_URL } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 500,
+});
 
 mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost:27017/moviesprojectdb', {
   useNewUrlParser: true,
@@ -18,6 +25,8 @@ mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost
   useUnifiedTopology: true,
 });
 app.use(requestLogger);
+app.use(limiter);
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(routes);
 app.use(errorLogger);
